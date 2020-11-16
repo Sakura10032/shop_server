b8a301743b7b2ef7b204f861d3117d886b66199d
Fahaxiki <wuliuqi10032@163.com>
2020-11-16 15:26:34 +0800

    feat：注册初始化

--- app/Http/Controllers/Api/InitController.php
+++ app/Http/Controllers/Api/InitController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\InitRequest;
+use App\Models\BaseSetting;
+use App\Models\Site;
+use Exception;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
@@ -11,17 +14,43 @@ class InitController extends Controller
 {
     // 初始化类
 
-    public function index(InitRequest $request)
+    public function index(InitRequest $request, BaseSetting $baseSetting, Site $site)
     {
         $registerData = Cache::get($request->register_key);
-
 
         if (!$registerData) {
             abort(403, '初始化注册码无效，请联系管理员！');
         }
 
+        $site = $site->find($registerData['site_id']);
+        if ($site->initialize === 1) {
+            abort(403, '请勿重复初始化！');
+        }
+
         // 启用事务
         DB::beginTransaction();
-        dd($registerData);
+        try {
+            $baseSetting->site_id = $registerData['site_id'];
+            $baseSetting->logo = '11'; // 默认初始化logo地址
+            $baseSetting->ico = '11'; // 默认初始化ico地址
+            $baseSetting->search_message = '请输入产品名称~'; // 产品搜索提示语
+            $baseSetting->copyright = '朗正集成信息科技（南京）有限公司'; // 版权信息
+            $baseSetting->bolg_copyright = '朗正集成信息科技（南京）有限公司'; // 博客版权信息
+            $baseSetting->company = '朗正集成信息科技（南京）有限公司'; // 公司名称
+            $baseSetting->save();
+            $site->initialize = 1;
+            $site->save();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 403);
+        }
+
+        return response()->json([
+            'message' => '初始化成功！'
+        ], 201);
+
     }
 }
---
+++ app/Models/BaseSetting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseSetting extends Model
+{
+    protected $table = 'base_settings';
+
+    public $timestamps = true;
+}
